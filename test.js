be326bfdf7ded93a126ccc267108e0643611218d
elifoguzkan <oguzkanelif@gmail.com>
2023-07-17 01:55:46 +0300

    My first commit

---
+++ test.js
@@ -0,0 +1,8 @@
+// Kullanıcıdan isim al
+const isim = "Lütfen isminizi girin:";
+
+// İsimle birlikte mesajı oluştur
+const mesaj = `Merhaba, ${isim}!`;
+
+// Mesajı konsola yazdır
+console.log(mesaj);
